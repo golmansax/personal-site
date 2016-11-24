1cc6215a58695238507381812eb41a27916a41be
Holman Gao <holman@golmansax.com>
2016-11-24 17:08:04 -0500

    Remove phenomic extras

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Page from "../layouts/Page"
+
+const HomePage = () => (
+  <Page head={{ title: 'Holman Gao' }}>
+    Holman Gao
+  </Page>
+)
+
+export default HomePage
