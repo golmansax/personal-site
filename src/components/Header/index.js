e9602afed77f185d7b085c2f5954cb88db216cb3
Holman Gao <holman@golmansax.com>
2016-11-25 21:44:27 -0500

    Appease lint

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,9 @@
-import React, { PropTypes } from "react"
-import { Link } from "phenomic"
-import Svg from "react-svg-inline"
-
-import twitterSvg from "../icons/iconmonstr-twitter-1.svg"
-import gitHubSvg from "../icons/iconmonstr-github-1.svg"
+import React from "react"
 
 import styles from "./index.css"
 
-const Header = (props, { metadata: { pkg } }) => (
-  <header className={ styles.header }>
-  </header>
+const Header = () => (
+  <header className={ styles.header } />
 )
-
-Header.contextTypes = {
-  metadata: PropTypes.object.isRequired,
-}
 
 export default Header
