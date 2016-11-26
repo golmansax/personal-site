c229d6a33d181578728d9b4bcc46467a8dac0a10
Holman Gao <holman@golmansax.com>
2016-11-25 22:01:58 -0500

    Add template content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,12 @@ import Page from "../layouts/Page"
 const HomePage = () => (
   <Page head={{ title: 'Holman Gao' }}>
     Holman Gao
+    I am an entrepenuer and freelance developer with a love for education.
+
+    Read my blog
+    Check out my portfolio
+    Tweet at me
+    Want to work together?
   </Page>
 )
 
