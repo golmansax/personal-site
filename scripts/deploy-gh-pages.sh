38a7c7e54c46d20209977d13fddd2079e026a442
Holman Gao <holman@golmansax.com>
2017-01-17 12:49:51 -0500

    Deploy to gh-pages

---
+++ scripts/deploy-gh-pages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+cd dist
+git init
+
+git config user.name "Holman Gao"
+git config user.email "holman@golmansax.com"
+
+git add .
+git commit -m 'Deploy to Github Pages'
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+
+# git rm CNAME
+# git commit -m 'Remove CNAME'
+git push --force --quiet "https://${GH_TOKEN}@github.com/golmansax/personal-site.git" master:gh-pages
