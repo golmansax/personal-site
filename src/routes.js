b822a0eac108f3c9f46c1c806ba7509f1a108e99
Holman Gao <holman@golmansax.com>
2017-01-31 01:55:35 -0500

    Remove intl resume routes for now

--- src/routes.js
+++ src/routes.js
@@ -7,21 +7,10 @@ import './highlight.global.css';
 import HomePage from './pages/HomePage';
 import ResumePage from './pages/ResumePage';
 
-const routes = (
-  <Route>
-    <IndexRoute component={HomePage} />
-    <Route path='resume' component={ResumePage} />
-  </Route>
-);
-
 export default (
   <Route path='/'>
-    {routes}
-    <Route path='es'>
-      {routes}
-    </Route>
-    <Route path='zh'>
-      {routes}
-    </Route>
+    <IndexRoute component={HomePage} />
+    <Route path='resume' component={ResumePage} />
+    <Route path='es' component={HomePage} />
   </Route>
 );
