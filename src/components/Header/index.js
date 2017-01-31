c9df1732fffc9d69e2057985ccaedcd69ce50c20
Holman Gao <holman@golmansax.com>
2017-01-31 01:22:50 -0500

    Add link to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,12 +6,14 @@ import styles from './index.css';
 const Header = () => (
   <header className={styles.header}>
     <h1 className={styles.heading}>
-      <FormattedMessage id='header.name' />
-      <small className={styles.subHeading}>
-        <FormattedMessage id='header.job1' /> |{' '}
-        <FormattedMessage id='header.job2' /> |{' '}
-        <span className={styles.chunk}><FormattedMessage id='header.location' /></span>
-      </small>
+      <a href='/' className={styles.link}>
+        <FormattedMessage id='header.name' />
+        <small className={styles.subHeading}>
+          <FormattedMessage id='header.job1' /> |{' '}
+          <FormattedMessage id='header.job2' /> |{' '}
+          <span className={styles.chunk}><FormattedMessage id='header.location' /></span>
+        </small>
+      </a>
     </h1>
   </header>
 );
