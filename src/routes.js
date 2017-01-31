fae8877f017c26a7bcba2f46ae358beb004b2fac
Holman Gao <holman@golmansax.com>
2017-01-31 01:36:24 -0500

    Try routes component rather than array

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,12 @@ import './highlight.global.css';
 import HomePage from './pages/HomePage';
 import ResumePage from './pages/ResumePage';
 
-const routes = [
-  <IndexRoute key='index' component={HomePage} />,
-  <Route key='resume' path='resume' component={ResumePage} />,
-];
+const routes = (
+  <Route>
+    <IndexRoute component={HomePage} />
+    <Route path='resume' component={ResumePage} />
+  </Route>
+);
 
 export default (
   <Route path='/'>
