eefa876a67ec5f5b60de5966f0f539a921d60f64
Holman Gao <holman@golmansax.com>
2017-10-20 00:05:52 -0400

    Update resume

--- src/pages/ResumePage/index.js
+++ src/pages/ResumePage/index.js
@@ -4,9 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import Page from '../../layouts/Page';
 import styles from './index.css';
 
-const DOC_ID = '0BzLh8cg8kVzCa093RmtkYkFoZG8';
-const URL = 'https://docs.google.com/viewer?' +
-  `srcid=${DOC_ID}&pid=explorer&efh=false&a=v&chrome=false&embedded=true`;
+const URL = '/public/resumes/gao_holman_resume_20171019.pdf';
 
 const ResumePage = () => (
   <Page
